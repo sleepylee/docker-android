9c740f36294ef4bc50d17a9017272e71443cc5a0
sleepylee <cs.leetrung@gmail.com>
2018-05-18 10:17:30 +0700

    Add Docker installation script - Yes always

--- setup-docker-ce.sh
+++ setup-docker-ce.sh
@@ -1,8 +1,8 @@
 # Update the apt package index:
-sudo apt-get update
+yes Y | sudo apt-get update
 
 # Install packages to allow apt to use a repository over HTTPS:
-sudo apt-get install \
+yes Y |sudo apt-get install \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -18,10 +18,10 @@ sudo add-apt-repository \
    stable"
 
 # Install latest version of Docker CE
-sudo apt-get install docker-ce
+yes Y | sudo apt-get install docker-ce
 
 # Install latest version of Docker Compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+yes Y | sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 
 # Apply executable permissions to the binary
 sudo chmod +x /usr/local/bin/docker-compose
