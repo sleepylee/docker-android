9553795f5dc9d487c15f15ac338a1f6015f39e4c
sleepylee <cs.leetrung@gmail.com>
2018-05-18 10:12:44 +0700

    Add Docker installation script

---
+++ setup-docker-ce.sh
@@ -0,0 +1,32 @@
+# Update the apt package index:
+sudo apt-get update
+
+# Install packages to allow apt to use a repository over HTTPS:
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+# Add Docker’s official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Set up the stable repository
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+# Install latest version of Docker CE
+sudo apt-get install docker-ce
+
+# Install latest version of Docker Compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+
+# Apply executable permissions to the binary
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Test
+docker --version
+docker-compose --version
+
